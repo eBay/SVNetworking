be9444d47173020d80c5225ebb22788eca5cca86
Nate Stedman <natesm@gmail.com>
2014-03-14 15:43:41 -0400

    Add SVNetworking project.

---
+++ SVNetworking/SVNetworking/SVNetworking.h
@@ -0,0 +1,13 @@
+//
+//  SVNetworking.h
+//  SVNetworking
+//
+//  Created by Nate Stedman on 3/14/14.
+//  Copyright (c) 2014 Svpply. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SVNetworking : NSObject
+
+@end
---
+++ SVNetworking/SVNetworking/SVNetworking.m
@@ -0,0 +1,13 @@
+//
+//  SVNetworking.m
+//  SVNetworking
+//
+//  Created by Nate Stedman on 3/14/14.
+//  Copyright (c) 2014 Svpply. All rights reserved.
+//
+
+#import "SVNetworking.h"
+
+@implementation SVNetworking
+
+@end
