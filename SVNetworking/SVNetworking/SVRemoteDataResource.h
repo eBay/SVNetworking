60a9f3d6130fde7421d5cf2b4154fac0a926ce91
Nate Stedman <natesm@gmail.com>
2014-03-17 17:15:59 -0400

    Add SVRemoteDataResource.

---
+++ SVNetworking/SVNetworking/SVRemoteDataResource.h
@@ -0,0 +1,36 @@
+//
+//  SVRemoteDataResource.h
+//  SVNetworking
+//
+//  Created by Nate Stedman on 3/17/14.
+//  Copyright (c) 2014 Svpply. All rights reserved.
+//
+
+#import "SVRemoteDataRequestResource.h"
+
+@interface SVRemoteDataResource : SVRemoteDataRequestResource
+
+#pragma mark - Access
+/**
+ Returns an (in-memory) cached remote data for the specified URL.
+ */
++(instancetype)cachedRemoteDataForURL:(NSURL*)URL;
+
+/**
+ Returns a remote data for the specified URL.
+ */
++(instancetype)remoteDataForURL:(NSURL*)URL;
+
+#pragma mark - URL
+/**
+ The URL to load data from.
+ */
+@property (nonatomic, readonly, strong) NSURL *URL;
+
+#pragma mark - Data
+/**
+ The loaded data. This property is observable.
+ */
+@property (nonatomic, readonly, strong) NSData* data;
+
+@end
---
+++ SVNetworking/SVNetworking/SVRemoteDataResource.m
@@ -0,0 +1,47 @@
+//
+//  SVRemoteDataResource.m
+//  SVNetworking
+//
+//  Created by Nate Stedman on 3/17/14.
+//  Copyright (c) 2014 Svpply. All rights reserved.
+//
+
+#import "SVRemoteDataResource.h"
+
+@interface SVRemoteDataResource ()
+
+#pragma mark - URL
+@property (nonatomic, strong) NSURL *URL;
+
+#pragma mark - Data
+@property (nonatomic, strong) NSData* data;
+
+@end
+
+@implementation SVRemoteDataResource
+
+#pragma mark - Access
++(instancetype)cachedRemoteDataForURL:(NSURL*)URL
+{
+    return [self cachedResourceWithUniqueKey:URL.absoluteString];
+}
+
++(instancetype)remoteDataForURL:(NSURL*)URL
+{
+    return [self resourceWithUniqueKey:URL.absoluteString withInitializationBlock:^(SVRemoteDataResource *resource) {
+        resource.URL = URL;
+    }];
+}
+
+#pragma mark - Implementation - Network Loading
+-(SVDataRequest*)requestForNetworkLoading
+{
+    return [SVDataRequest GETRequestWithURL:_URL];
+}
+
+-(void)parseFinishedData:(NSData*)data error:(NSError**)error
+{
+    self.data = data;
+}
+
+@end
