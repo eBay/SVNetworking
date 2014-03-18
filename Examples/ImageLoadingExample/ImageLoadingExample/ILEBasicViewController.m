c6774daecede8920424e8b5159a8ee76cc5e78b5
Nate Stedman <natesm@gmail.com>
2014-03-18 14:17:14 -0400

    Add switch for screen scaling to basic controller.

--- Examples/ImageLoadingExample/ImageLoadingExample/ILEBasicViewController.m
+++ Examples/ImageLoadingExample/ImageLoadingExample/ILEBasicViewController.m
@@ -16,6 +16,8 @@
 @private
     IBOutlet ILERemoteImageView *_topView;
     IBOutlet ILERemoteImageView *_bottomView;
+    IBOutlet UISwitch *_scalingSwitch;
+    NSURL *_URL;
 }
 
 @end
@@ -26,10 +28,24 @@
 {
     [super viewDidLoad];
     
-    NSURL *URL = [NSURL URLWithString:@"http://s3-ec.buzzfed.com/static/2013-12/enhanced/webdr03/5/11/enhanced-buzz-22866-1386260133-20.jpg"];
+    _URL = [NSURL URLWithString:@"http://s3-ec.buzzfed.com/static/2013-12/enhanced/webdr03/5/11/enhanced-buzz-22866-1386260133-20.jpg"];
     
-    _topView.remoteImage = [SVRemoteImage remoteImageForURL:URL];
-    _bottomView.remoteImage = [SVRemoteScaledImage remoteScaledImageForURL:URL withSize:_bottomView.bounds.size];
+    _topView.remoteImage = [SVRemoteImage remoteImageForURL:_URL];
+    _bottomView.remoteImage = [SVRemoteScaledImage remoteScaledImageForURL:_URL withSize:_bottomView.bounds.size];
+}
+
+-(IBAction)scalingSwitchAction:(id)sender
+{
+    if (_scalingSwitch.on)
+    {
+        _bottomView.remoteImage = [SVRemoteScaledImage remoteScaledImageForURL:_URL
+                                                                     withScale:[UIScreen mainScreen].scale
+                                                                          size:_bottomView.bounds.size];
+    }
+    else
+    {
+        _bottomView.remoteImage = [SVRemoteScaledImage remoteScaledImageForURL:_URL withSize:_bottomView.bounds.size];
+    }
 }
 
 @end
