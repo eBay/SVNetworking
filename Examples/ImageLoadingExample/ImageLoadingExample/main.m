800e3dc69cf6177474aab838e25eb377919198df
Nate Stedman <natesm@gmail.com>
2014-03-18 12:07:22 -0400

    Add image loading example project.

---
+++ Examples/ImageLoadingExample/ImageLoadingExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ImageLoadingExample
+//
+//  Created by Nate Stedman on 3/18/14.
+//  Copyright (c) 2014 Svpply. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ILEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ILEAppDelegate class]));
+    }
+}
