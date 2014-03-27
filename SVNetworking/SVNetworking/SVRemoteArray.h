1f24cad6700d870c1cf4669099245bc65807172c
Nate Stedman <natesm@gmail.com>
2014-03-27 19:12:52 -0400

    SVRemoteArray implementation.

--- SVNetworking/SVNetworking/SVRemoteArray.h
+++ SVNetworking/SVNetworking/SVRemoteArray.h
@@ -16,7 +16,7 @@
 
 // page loading
 -(void)remoteArray:(SVRemoteArray*)remoteArray didRefreshWithItems:(NSArray*)items;
--(void)remoteArray:(SVRemoteArray*)remoteArray didLoadNextPageWithItems:(NSArray*)items;
+-(void)remoteArray:(SVRemoteArray*)remoteArray didLoadNextPageWithItems:(NSArray*)items hasNextPage:(BOOL)hasNextPage;
 
 // error handling
 -(void)remoteArray:(SVRemoteArray*)remoteArray didFailToRefreshWithError:(NSError*)error;
@@ -33,9 +33,34 @@
 -(id)objectAtIndexedSubscript:(NSUInteger)index;
 @property (nonatomic, readonly) NSUInteger count;
 
+#pragma mark - Loading Instructions
+-(void)loadNextPage;
+-(void)refresh;
+
+#pragma mark - Loading State
+@property (nonatomic, readonly) BOOL isLoadingNextPage;
+@property (nonatomic, readonly) BOOL isRefreshing;
+@property (nonatomic, readonly) BOOL hasNextPage;
+
+#pragma mark - Loading Errors
+@property (nonatomic, readonly, strong) NSError *nextPageError;
+@property (nonatomic, readonly, strong) NSError *refreshError;
+
 #pragma mark - Pagination Observers
 -(void)addPaginationObserver:(id<SVRemoteArrayPaginationObserver>)paginationObserver;
 -(void)removePaginationObserver:(id<SVRemoteArrayPaginationObserver>)paginationObserver;
 -(void)enumeratePaginationObservers:(void(^)(id<SVRemoteArrayPaginationObserver> paginationObserver))block;
+
+#pragma mark - Implementation - Abstract (Subclasses)
+-(void)beginLoadingNextPage;
+-(void)beginRefreshing;
+
+#pragma mark - Implementation - Next Page
+-(void)finishLoadingNextPageWithItems:(NSArray*)items hasNextPage:(BOOL)hasNextPage;
+-(void)failLoadingNextPageWithError:(NSError*)error;
+
+#pragma mark - Implementation - Refresh
+-(void)finishRefreshingWithItems:(NSArray*)items;
+-(void)failRefreshingWithError:(NSError*)error;
 
 @end
--- SVNetworking/SVNetworking/SVRemoteArray.m
+++ SVNetworking/SVNetworking/SVRemoteArray.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2014 Svpply. All rights reserved.
 //
 
+#import "NSObject+SVBindings.h"
+#import "SVFunctional.h"
 #import "SVRemoteArray.h"
 
 @interface SVRemoteArray ()
@@ -13,6 +15,18 @@
 @private
     NSHashTable *_paginationObservers;
 }
+
+#pragma mark - Contents
+@property (nonatomic, strong) NSArray *contents;
+
+#pragma mark - Loading State
+@property (nonatomic) BOOL isLoadingNextPage;
+@property (nonatomic) BOOL isRefreshing;
+@property (nonatomic) BOOL hasNextPage;
+
+#pragma mark - Loading Errors
+@property (nonatomic, strong) NSError *nextPageError;
+@property (nonatomic, strong) NSError *refreshError;
 
 @end
 
@@ -27,6 +41,7 @@
     {
         _contents = @[];
         _paginationObservers = [NSHashTable weakObjectsHashTable];
+        _hasNextPage = YES;
     }
     
     return self;
@@ -41,6 +56,38 @@
 -(NSUInteger)count
 {
     return _contents.count;
+}
+
+#pragma mark - Loading
+-(void)loadNextPage
+{
+    if (!self.isLoadingNextPage && self.hasNextPage)
+    {
+        // clear next page error if necessary
+        if (_nextPageError)
+        {
+            self.nextPageError = nil;
+        }
+        
+        // load the next page
+        [self beginLoadingNextPage];
+    }
+}
+
+-(void)refresh
+{
+    // we can only refresh if we've loaded the first page, or loaded an empty first page
+    if ((self.count > 0 || !self.hasNextPage) && !self.isRefreshing)
+    {
+        // clear refresh error if necessary
+        if (_refreshError)
+        {
+            self.refreshError = nil;
+        }
+        
+        // refresh
+        [self beginRefreshing];
+    }
 }
 
 #pragma mark - Pagination Observers
@@ -60,6 +107,80 @@
     {
         block(paginationObserver);
     }
+}
+
+#pragma mark - Implementation - Abstract (Subclasses)
+-(void)beginLoadingNextPage
+{
+    [self doesNotRecognizeSelector:_cmd];
+}
+
+-(void)beginRefreshing
+{
+    [self doesNotRecognizeSelector:_cmd];
+}
+
+#pragma mark - Implementation - Next Page
+-(void)finishLoadingNextPageWithItems:(NSArray*)items hasNextPage:(BOOL)hasNextPage
+{
+    [self willChangeValueForKey:SV_KEYPATH(self, count)];
+    self.contents = [self.contents arrayByAddingObjectsFromArray:items];
+    [self didChangeValueForKey:SV_KEYPATH(self, count)];
+    
+    if (_hasNextPage != hasNextPage)
+    {
+        self.hasNextPage = hasNextPage;
+    }
+    
+    [self enumeratePaginationObservers:^(id<SVRemoteArrayPaginationObserver> paginationObserver) {
+        if ([paginationObserver respondsToSelector:@selector(remoteArray:didLoadNextPageWithItems:hasNextPage:)])
+        {
+            [paginationObserver remoteArray:self didLoadNextPageWithItems:items hasNextPage:hasNextPage];
+        }
+    }];
+}
+
+-(void)failLoadingNextPageWithError:(NSError*)error
+{
+    self.nextPageError = error;
+    self.isLoadingNextPage = NO;
+    
+    [self enumeratePaginationObservers:^(id<SVRemoteArrayPaginationObserver> paginationObserver) {
+        if ([paginationObserver respondsToSelector:@selector(remoteArray:didFailToLoadNextPageWithError:)])
+        {
+            [paginationObserver remoteArray:self didFailToLoadNextPageWithError:error];
+        }
+    }];
+}
+
+#pragma mark - Implementation - Refresh
+-(void)finishRefreshingWithItems:(NSArray*)items
+{
+    [self willChangeValueForKey:SV_KEYPATH(self, count)];
+    self.contents = [items arrayByAddingObjectsFromArray:self.contents];
+    [self didChangeValueForKey:SV_KEYPATH(self, count)];
+    
+    self.isRefreshing = NO;
+    
+    [self enumeratePaginationObservers:^(id<SVRemoteArrayPaginationObserver> paginationObserver) {
+        if ([paginationObserver respondsToSelector:@selector(remoteArray:didRefreshWithItems:)])
+        {
+            [paginationObserver remoteArray:self didRefreshWithItems:items];
+        }
+    }];
+}
+
+-(void)failRefreshingWithError:(NSError*)error
+{
+    self.refreshError = error;
+    self.isRefreshing = NO;
+    
+    [self enumeratePaginationObservers:^(id<SVRemoteArrayPaginationObserver> paginationObserver) {
+        if ([paginationObserver respondsToSelector:@selector(remoteArray:didFailToRefreshWithError:)])
+        {
+            [paginationObserver remoteArray:self didFailToRefreshWithError:error];
+        }
+    }];
 }
 
 @end
