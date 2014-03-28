59d716f84ce6134262fd725b52a105fa7c4af9b7
Nate Stedman <natesm@gmail.com>
2014-03-28 12:10:28 -0400

    SVRemoteJSONArray documentation.

--- SVNetworking/SVNetworking/SVRemoteJSONArray.h
+++ SVNetworking/SVNetworking/SVRemoteJSONArray.h
@@ -15,7 +15,23 @@ typedef enum
     SVRemoteJSONArrayLoadingTypeNextPage
 } SVRemoteJSONArrayLoadingType;
 
+/**
+ An abstract implementation of a remote array, using JSON requests to load its data.
+ */
 @interface SVRemoteJSONArray : SVRemoteArray
+
+#pragma mark - Requests
+/** @name Requests */
+
+/**
+ Allows subclasses to customize the next page request. The default implementation returns the result of -buildRequest.
+ */
+-(SVJSONRequest*)buildNextPageRequest;
+
+/**
+ Allows subclasses to customize the refresh request. The default implementation returns the result of -buildRequest.
+ */
+-(SVJSONRequest*)buildRefreshRequest;
 
 #pragma mark - Requests - Abstract
 /** @name Requests - Abstract */
@@ -28,17 +44,6 @@ typedef enum
  */
 -(SVJSONRequest*)buildRequest;
 
-#pragma mark - Requests
-/**
- Allows subclasses to customize the next page request. The default implementation returns the result of -buildRequest.
- */
--(SVJSONRequest*)buildNextPageRequest;
-
-/**
- Allows subclasses to customize the refresh request. The default implementation returns the result of -buildRequest.
- */
--(SVJSONRequest*)buildRefreshRequest;
-
 #pragma mark - Parsing
 /** @name Parsing */
 
@@ -49,6 +54,10 @@ typedef enum
  
  Note that for well-designed APIs, this can also be easily achieved with a subclass of SVJSONRequest, which should be
  much more reusable, and therefore is preferred.
+ 
+ @param JSON The JSON object to validate.
+ @param error An error pointer to set if the JSON is invalid.
+ @returns `YES` for valid JSON, otherwise `NO`.
  */
 -(BOOL)checkJSON:(id)JSON error:(__autoreleasing NSError**)error;
 
@@ -57,11 +66,20 @@ typedef enum
 
 /**
  Subclasses must override this message to parse JSON data into items.
+ 
+ @param JSON The JSON object loaded for this page (or refresh).
+ @param loadingType If this is a refresh or next page operation.
+ @returns An array of parsed objects. If there is possible overlap with previously loaded items, it is the
+ responsibility of the subclass to trim this array appropriately.
  */
 -(NSArray*)itemsForJSON:(id)JSON withLoadingType:(SVRemoteJSONArrayLoadingType)loadingType;
 
 /**
  Subclasses must override this message to determine if the array has an additional page.
+ 
+ @param JSON The JSON object loaded for this page.
+ @param items The parsed items for this page.
+ @returns `YES` if the remote array has an additional page, otherwise `NO`.
  */
 -(BOOL)hasNextPageForJSON:(id)JSON withParsedItems:(NSArray*)items;
 
