0da39f1026b2de95a6c862e2dbc7e197fb29b535
Nate Stedman <natesm@gmail.com>
2014-03-28 11:56:04 -0400

    Documentation for request objects.

--- SVNetworking/SVNetworking/SVDataRequest.h
+++ SVNetworking/SVNetworking/SVDataRequest.h
@@ -2,14 +2,30 @@
 
 @class SVDataRequest;
 
+/**
+ A delegate protocol for SVDataRequest objects.
+ */
 @protocol SVDataRequestDelegate <SVRequestDelegate>
 
+/**
+ Sent when the request finishes successfully.
+ 
+ @param request The request.
+ @param data The data object loaded by the request.
+ @param response The HTTP response received by the request.
+ */
 -(void)request:(SVDataRequest*)request finishedWithData:(NSData*)data response:(NSHTTPURLResponse*)response;
 
 @end
 
+/**
+ A request that loads data objects.
+ */
 @interface SVDataRequest : SVRequest
 
+/**
+ The delegate for the request.
+ */
 @property (readwrite, weak) id<SVDataRequestDelegate> delegate;
 
 @end
