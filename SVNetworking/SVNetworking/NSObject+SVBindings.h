2dfa0fadefc4507c052bc6961a56063bdd414076
Nate Stedman <natesm@gmail.com>
2014-03-28 10:55:56 -0400

    NSObject+SVBindings documentation.

--- SVNetworking/SVNetworking/NSObject+SVBindings.h
+++ SVNetworking/SVNetworking/NSObject+SVBindings.h
@@ -5,14 +5,58 @@
 
 typedef id(^SVBindingBlock)(id value);
 
+/**
+ A minimal KVO-based property binding system for NSObject subclasses.
+ */
 @interface NSObject (SVBindings)
 
 #pragma mark - Binding
+/** @name Binding */
+
+/**
+ Binds a key path.
+ 
+ When the target key path changes, the key path of this object will be automatically set.
+ 
+ Bindings must be removed when this object is deallocated. The most efficient way to do this is -sv_unbindAll.
+ 
+ @param keyPath The key path to bind.
+ @param object The object to bind to.
+ @param boundKeyPath The key path to bind to.
+ */
 -(void)sv_bind:(NSString*)keyPath toObject:(id)object withKeyPath:(NSString*)boundKeyPath;
+
+/**
+ Binds a key path with a transformation block.
+ 
+ When the target key path changes, the key path of this object will be automatically set.
+ 
+ Bindings must be removed when this object is deallocated. The most efficient way to do this is -sv_unbindAll.
+ 
+ @param keyPath The key path to bind.
+ @param object The object to bind to.
+ @param boundKeyPath The key path to bind to.
+ @param block A block that the new value of boundKeyPath will be passed to. The receiver's key path will be set to the
+ value returned from the block.
+ */
 -(void)sv_bind:(NSString*)keyPath toObject:(id)object withKeyPath:(NSString*)boundKeyPath block:(SVBindingBlock)block;
 
 #pragma mark - Unbinding
+/** @name Unbinding */
+
+/**
+ Unbinds a key path.
+ 
+ This message is less efficient than -sv_unbindAll. When cleaning up all bindings for an object during deallocation, use
+ that message instead.
+ 
+ @param keyPath The key path to unbind.
+ */
 -(void)sv_unbind:(NSString*)keyPath;
+
+/**
+ Removes all key path bindings.
+ */
 -(void)sv_unbindAll;
 
 @end
