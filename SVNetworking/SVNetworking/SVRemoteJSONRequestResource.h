0aab047d5f767e0d6cc58ce45dbc185766351c61
Nate Stedman <natesm@gmail.com>
2014-03-28 11:20:12 -0400

    SVRemoteJSONRequestResource documentation.

--- SVNetworking/SVNetworking/SVRemoteJSONRequestResource.h
+++ SVNetworking/SVNetworking/SVRemoteJSONRequestResource.h
@@ -16,6 +16,8 @@
 @interface SVRemoteJSONRequestResource : SVRemoteResource
 
 #pragma mark - Implementation
+/** @name Implementation */
+
 /**
  After JSON loading is complete, this message will be passed. Subclasses generally will not need to override this
  message.
@@ -28,21 +30,31 @@
 /**
  A convenience message for subclasses using a data-based alternative loading method (i.e. a disk cache). This message
  will parse the passed data and pass -finishLoadingWithJSON: or -failLoadingWithError: as is appropriate.
+ 
+ @param JSONData A data object to be parsed as JSON.
  */
 -(void)finishLoadingWithJSONData:(NSData*)JSONData;
 
 #pragma mark - Subclass Implementation
+/** @name Subclass Implementation */
+
 /**
  Subclasses must override this message to provide a JSON request to load themselves.
  
- The default implementation throws an exception.
+ @warning The default implementation throws an exception.
+ 
+ @returns A valid JSON request for loading this resource.
  */
 -(SVJSONRequest*)requestForNetworkLoading;
 
 /**
  Subclasses must override this message to parse loaded JSON (and set completion properties) or set the error pointer.
  
- The default implementation throws an exception.
+ @warning The default implementation throws an exception.
+ 
+ @param JSON The JSON to parse.
+ @param error An error out pointer, which should be set if the JSON is invalid.
+ @returns `YES` for a successful parse, otherwise `NO`.
  */
 -(BOOL)parseFinishedJSON:(id)JSON error:(__autoreleasing NSError**)error;
 
