1620c162da2fee6774fe0ed13e2fec2b05f6d7c1
Nate Stedman <natesm@gmail.com>
2014-03-28 11:25:08 -0400

    SVImageScaler documentation.

--- SVNetworking/SVNetworking/SVImageScaler.h
+++ SVNetworking/SVNetworking/SVImageScaler.h
@@ -15,13 +15,21 @@
 
 /**
  Synchronously scales an image to the specified size and scale.
+ 
+ @param image The source image.
+ @param size The destination size to fit to.
+ @param scale The `scale` of the destination image.
+ @returns A scaled image.
  */
 +(UIImage*)scaleImage:(UIImage*)image toSize:(CGSize)size withScale:(CGFloat)scale;
 
 /**
  Asynchronously scales an image to the specified size and scale.
  
- The completion handler will be called on the main thread.
+ @param image The source image.
+ @param size The destination size to fit to.
+ @param scale The `scale` of the destination image.
+ @param completion A completion handler, which will be called on the main thread.
  */
 +(void)scaleImage:(UIImage*)image toSize:(CGSize)size withScale:(CGFloat)scale completion:(void(^)(UIImage *scaledImage))completion;
 
