e9ee68deb0a2d9f4bb13dbc2f4b9bfaceba012f6
Nate Stedman <natesm@gmail.com>
2014-03-28 11:42:16 -0400

    SVRemoteScaledImageProtocol documentation.

--- SVNetworking/SVNetworking/SVRemoteScaledImageProtocol.h
+++ SVNetworking/SVNetworking/SVRemoteScaledImageProtocol.h
@@ -16,24 +16,44 @@
 @protocol SVRemoteScaledImageProtocol <SVRemoteImageProtocol>
 
 #pragma mark - Access
+/** @name Access */
+
 /**
  Returns an (in-memory) cached remote image for the specified URL, scaled to the specified size.
+ 
+ This image has an implicit `scale` of 1.
+ 
+ @param URL The URL for the image.
+ @param size The size to scale to.
  */
 +(instancetype)cachedRemoteScaledImageForURL:(NSURL*)URL withSize:(CGSize)size;
 
 /**
  Returns a remote image for the specified URL, scaled to the specified size;
+ 
+ This image has an implicit `scale` of 1.
+ 
+ @param URL The URL for the image.
+ @param size The size to scale to.
  */
 +(instancetype)remoteScaledImageForURL:(NSURL*)URL withSize:(CGSize)size;
 
 /**
  Returns an (in-memory) cached remote image for the specified URL, with the specified scale factor, scaled to the
  specified size.
+ 
+ @param URL The URL for the image.
+ @param scale The scale factor for the image.
+ @param size The size to scale to.
  */
 +(instancetype)cachedRemoteScaledImageForURL:(NSURL*)URL withScale:(CGFloat)scale size:(CGSize)size;
 
 /**
  Returns a remote image for the specified URL, with the specified scale factor, scaled to the specified size;
+ 
+ @param URL The URL for the image.
+ @param scale The scale factor for the image.
+ @param size The size to scale to.
  */
 +(instancetype)remoteScaledImageForURL:(NSURL*)URL withScale:(CGFloat)scale size:(CGSize)size;
 
