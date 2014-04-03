a9b6ed8f7bfdddfef12a615adc1e2563ca0f593e
Nate Stedman <natesm@gmail.com>
2014-04-03 12:53:57 -0400

    Documentation for cache.

--- SVNetworking/SVNetworking/SVRemoteResourceCache.h
+++ SVNetworking/SVNetworking/SVRemoteResourceCache.h
@@ -15,18 +15,43 @@ typedef void(^SVRemoteResourceCacheWriteCompletionBlock)(void);
 @class SVRemoteResource;
 
 /**
- Defines messages for a cached keyed by remote objects.
+ Defines messages for a data cache keyed by remote objects.
+ 
+ For an in-memory cache, use NSCache to store actual remote resource instances.
+ 
+ SVRemoteResourceDiskCache is an implementation of this protocol.
  */
 @protocol SVRemoteResourceCache <NSObject>
 
 #pragma mark - Reading
 /** @name Reading */
+
+/**
+ Reads data from the cache.
+ 
+ @param remoteResource The resource to read data for.
+ @param completion A block to call after data is successfully loaded. The thread that this block should be called on is
+ not specified. This parameter may be `nil`.
+ @param failure A block to call if the cache does not contain data for the resource, or the data cannot be loaded. The
+ thread that this block should be called on is not specified. This parameter may be `nil`.
+ */
 -(void)dataForResource:(SVRemoteResource*)remoteResource
             completion:(SVRemoteResourceCacheReadCompletionBlock)completion
                failure:(SVRemoteResourceCacheFailureBlock)failure;
 
 #pragma mark - Writing
 /** @name Writing */
+
+/**
+ Writes data to the cache.
+ 
+ @param data The data to write.
+ @param remoteResource The resource to key the data to.
+ @param completion A block to call upon successful write. The thread that this block should be called on is not
+ specified. This parameter may be `nil`.
+ @param failure A block to call if the write fails. The thread that this block should be called on is not specified.
+ This parameter may be `nil`.
+ */
 -(void)writeData:(NSData*)data
      forResource:(SVRemoteResource*)remoteResource
       completion:(SVRemoteResourceCacheWriteCompletionBlock)completion
--- SVNetworking/SVNetworking/SVRemoteResourceDiskCache.h
+++ SVNetworking/SVNetworking/SVRemoteResourceDiskCache.h
@@ -1,6 +1,14 @@
 #import "SVRemoteResourceCache.h"
 
+/**
+ A disk cache for remote resources.
+ 
+ The resource's -[SVRemoteResource uniqueKeyHash] is used as a filename for items in the cache.
+ */
 @interface SVRemoteResourceDiskCache : NSObject <SVRemoteResourceCache>
+
+#pragma mark - Initialization
+/** @name Initialization */
 
 /**
  Initializes a disk cache instance.
@@ -10,6 +18,8 @@
 -(instancetype)initWithFileURL:(NSURL *)fileURL;
 
 #pragma mark - IO Queue
+/** @name IO Queue */
+
 /**
  The IO queue to perform data reads and writes on.
  
