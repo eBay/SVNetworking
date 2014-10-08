d01ab902d50320148449971cdf5c0dc925b68bb1
Nate Stedman <natesm@gmail.com>
2014-10-08 13:08:29 -0400

    SVNetworking Swift version - created project.

---
+++ Swift/SVNetworking/SVNetworking/SVNetworking.h
@@ -0,0 +1,19 @@
+//
+//  SVNetworking.h
+//  SVNetworking
+//
+//  Created by Nate Stedman on 10/8/14.
+//  Copyright (c) 2014 eBay. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SVNetworking.
+FOUNDATION_EXPORT double SVNetworkingVersionNumber;
+
+//! Project version string for SVNetworking.
+FOUNDATION_EXPORT const unsigned char SVNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVNetworking/PublicHeader.h>
+
+
